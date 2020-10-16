3ad9b275b837554d2e3d2f5264123725a8913d1c
OnceDoter <gordeev_p@mail.ru>
2020-10-16 16:24:55 +0700

    links works

--- pixelpark/src/components/Сontact.tsx
+++ pixelpark/src/components/Сontact.tsx
@@ -6,81 +6,53 @@ import InstagramIcon from '@material-ui/icons/Instagram'
 import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import {Grid} from "@material-ui/core";
-import {Link} from 'react-router-dom'
-import {Router} from "@material-ui/icons";
 import {environment} from "../environment/environment";
 
 export default function Contact() {
-
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+    const onClick = (ref : string) => window.location.href = ref
 
     return (
         <div>
-            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                Open Menu
-            </Button>
             <Menu
                 id="simple-menu"
-                anchorEl={anchorEl}
                 keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
+                open={true}
             >
                 <MenuItem>
-                    <Router>
-                        <Link to={environment.GitHubProfile}>
-                            <Button>
-                                <Grid container spacing={2}>
-                                    <Grid item xs>
-                                        <GitHubIcon fontSize="small"/>
-                                    </Grid>
-                                    <Grid item>
-                                        Show me in github!
-                                    </Grid>
-                                </Grid>
-                            </Button>
-                        </Link>
-                    </Router>
+                    <Button onClick={()=>onClick(environment.GitHubProfile)}>
+                        <Grid container spacing={2}>
+                            <Grid item xs>
+                                <GitHubIcon fontSize="small"/>
+                            </Grid>
+                            <Grid item>
+                                Show me in github!
+                            </Grid>
+                        </Grid>
+                    </Button>
                 </MenuItem>
                 <MenuItem>
-                    <Router>
-                        <Link to={environment.MailProfile}>
-                            <Button>
-                                <Grid container spacing={2}>
-                                    <Grid item xs>
-                                        <AlternateEmailIcon fontSize="small"/>
-                                    </Grid>
-                                    <Grid item>
-                                        My world mail.ru
-                                    </Grid>
-                                </Grid>
-                            </Button>
-                        </Link>
-                    </Router>
+                    <Button onClick={()=>onClick(environment.MailProfile)}>
+                        <Grid container spacing={2}>
+                            <Grid item xs>
+                                <AlternateEmailIcon fontSize="small"/>
+                            </Grid>
+                            <Grid item>
+                                My world mail.ru
+                            </Grid>
+                        </Grid>
+                    </Button>
                 </MenuItem>
                 <MenuItem>
-                    <Router>
-                        <Link to={environment.InstagramProfile}>
-                            <Button>
-                                <Grid container spacing={2}>
-                                    <Grid item xs>
-                                        <InstagramIcon fontSize="small"/>
-                                    </Grid>
-                                    <Grid item>
-                                        Godlike instagram
-                                    </Grid>
-                                </Grid>
-                            </Button>
-                        </Link>
-                    </Router>
+                    <Button onClick={()=>onClick(environment.InstagramProfile)}>
+                        <Grid container spacing={2}>
+                            <Grid item xs>
+                                <InstagramIcon fontSize="small"/>
+                            </Grid>
+                            <Grid item>
+                                Godlike instagram
+                            </Grid>
+                        </Grid>
+                    </Button>
                 </MenuItem>
             </Menu>
         </div>
